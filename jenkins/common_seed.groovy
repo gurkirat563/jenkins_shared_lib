7417b13096448f68f6f3a548db724969b3c54bcf
gurkirat563 <80724601+gurkirat563@users.noreply.github.com>
2021-09-23 21:54:11 -0400

    Update common_seed.groovy

--- jenkins/common_seed.groovy
+++ jenkins/common_seed.groovy
@@ -1,4 +1,4 @@
-def baseUrl = "https://github.com/ybmadhu/"
+def baseUrl = "https://github.com/gurkirat563/"
 def repoName = "$reponame"
 def gitRepoUrl= baseUrl + repoName + '.git'
 def jobName = "$reponame"
@@ -22,7 +22,7 @@ def jobName = "$reponame"
                     git {
                         remote {
                             url(gitRepoUrl)
-                            credentials('github_credentials')
+                            credentials('git-credentials')
                         }
                         branches('master')
                         extensions {
